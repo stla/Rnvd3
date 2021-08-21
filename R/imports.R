66342172cd484bc929e8943459323c6078a0978e
stla <laurent_step@yahoo.fr>
2021-08-21 13:30:46 +0200

    imports JS and saveWidget

---
+++ R/imports.R
@@ -0,0 +1,12 @@
+#' @title Objects imported from other packages
+#' @description These objects are imported from other packages.
+#'   Follow the links to their documentation:
+#'   \code{\link[htmlwidgets:JS]{JS}},
+#'   \code{\link[htmlwidgets:saveWidget]{saveWidget}}.
+#' @importFrom htmlwidgets JS saveWidget
+#' @export JS saveWidget
+#' @name Rnvd3-imports
+#' @aliases JS saveWidget
+#' @docType import
+NULL
+
