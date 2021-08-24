d7667c65736aeab5710728586c953ba267f5ed8e
stla <laurent_step@yahoo.fr>
2021-08-24 21:39:14 +0200

    date example

--- inst/essais/essai-linechart03_date.R
+++ inst/essais/essai-linechart03_date.R
@@ -1,13 +1,16 @@
 library(Rnvd3)
 
 dat1 <-
-  lineChartData(x = Sys.Date() + 1:10, y = sin(1:10/10), key = "Sine wave", color = "lime")
+  lineChartData(
+    x = Sys.Date() + 1:100, y = sin(1:100/10), key = "Sine wave", color = "lime"
+  )
 dat2 <-
-  lineChartData(x = Sys.Date() + 1:10, y = sin(1:10/10)*0.25 + 0.5,
-                key = "Another sine wave", color = "red")
+  lineChartData(x = Sys.Date() + 1:100, y = sin(1:100/10)*0.25 + 0.5,
+                key = "Another sine wave", color = "darkred")
 dat <- list(dat1, dat2)
 
-lineChart(dat, #yLabelsFontSize = "0.75rem",
-          interpolate = "bundle",
-          margins = list(t = 100, r = 100, b = 100, l = 100),
-          xAxisTickFormat = "%Y-%m-%d")
+lineChart(
+  dat,
+  margins = list(t = 100, r = 100, b = 100, l = 100),
+  xAxisTickFormat = "%Y-%m-%d"
+)
