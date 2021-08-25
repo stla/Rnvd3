8c4bb2db38c58091bbcdb6752be3695221529f44
stla <laurent_step@yahoo.fr>
2021-08-25 06:08:27 +0200

    xRange datetime

--- inst/essais/essai-linechart04_posixct.R
+++ inst/essais/essai-linechart04_posixct.R
@@ -1,6 +1,7 @@
 library(Rnvd3)
 
 x <- Sys.time() + (1:300)
+xRange <- c(Sys.time() - 100, Sys.time() + 400)
 
 dat1 <-
   lineChartData(
@@ -14,5 +15,6 @@ dat <- list(dat1, dat2)
 lineChart(
   dat,
   margins = list(t = 100, r = 100, b = 100, l = 100),
-  xAxisTickFormat = "%H:%M:%S"
+  xAxisTickFormat = "%H:%M:%S",
+  xRange = xRange
 )
