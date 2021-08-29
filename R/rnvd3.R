2b839515aa3e72c935e5fb33341b95f3f21d821d
stla <laurent_step@yahoo.fr>
2021-08-29 08:49:34 +0200

    datetime example

--- R/rnvd3.R
+++ R/rnvd3.R
@@ -473,6 +473,27 @@ hMultiBarChart <- function(
 #'   margins = list(t = 100, r = 100, b = 100, l = 100),
 #'   xAxisTickFormat = "%Y-%m-%d"
 #' )
+#'
+#' # with a datetime x-axis
+#'
+#' dat <- data.frame(
+#'   x  = Sys.time() + (1:300),
+#'   y1 = sin(1:300/10),
+#'   y2 = sin(1:300/10)*0.25 + 0.5
+#' )
+#' dat1 <-
+#'   lineChartData(x = ~x, y = ~y1, data = dat, key = "Sine wave", color = "lime")
+#' dat2 <-
+#'   lineChartData(x = ~x, y = ~y2, data = dat,
+#'                 key = "Another sine wave", color = "darkred")
+#' dat12 <- list(dat1, dat2)
+#'
+#' lineChart(
+#'   dat12,
+#'   margins = list(t = 100, r = 100, b = 100, l = 100),
+#'   xAxisTickFormat = "%H:%M:%S",
+#'   xAxisTitle = "Time", yAxisTitle = "Energy"
+#' )
 lineChart <- function(
   data,
   xAxisTitle = "x",
--- inst/essais/essai-linechart04_posixct.R
+++ inst/essais/essai-linechart04_posixct.R
@@ -1,19 +1,22 @@
 library(Rnvd3)
 
-x <- Sys.time() + (1:300)
+dat <- data.frame(
+  x  = Sys.time() + (1:300),
+  y1 = sin(1:300/10),
+  y2 = sin(1:300/10)*0.25 + 0.5
+)
+
 xRange <- c(Sys.time() - 100, Sys.time() + 400)
 
 dat1 <-
-  lineChartData(
-    x = x, y = sin(1:300/10), key = "Sine wave", color = "lime"
-  )
+  lineChartData(x = ~x, y = ~y1, data = dat, key = "Sine wave", color = "lime")
 dat2 <-
-  lineChartData(x = x, y = sin(1:300/10)*0.25 + 0.5,
+  lineChartData(x = ~x, y = ~y2, data = dat,
                 key = "Another sine wave", color = "darkred")
-dat <- list(dat1, dat2)
+dat12 <- list(dat1, dat2)
 
 lineChart(
-  dat,
+  dat12,
   margins = list(t = 100, r = 100, b = 100, l = 100),
   xAxisTickFormat = "%H:%M:%S",
   xRange = xRange
